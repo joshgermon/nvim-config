d4396162d773649e223bb18e1773b2a0a17b588c
joshgermon <joshgermon@gmail.com>
2023-04-15 23:52:09 +1000

    Add trouble workspace diagnostics

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,3 +1,6 @@
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
 vim.keymap.set("n", "<leader>qf", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
