36041a63b7dfde8eed0895afc0270981017531d9
Joshua Germon <joshgermon@gmail.com>
2023-08-03 22:46:34 +1000

    Latest updates: formatting, themes and removal

--- lua/joshgermon/packer.lua
+++ lua/joshgermon/packer.lua
@@ -5,15 +5,17 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+    use({ "typicode/bg.nvim" })
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
-    use('Mofiqul/dracula.nvim')
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
     use {
         'numToStr/Comment.nvim',
         config = function()
@@ -26,38 +28,36 @@ return require('packer').startup(function(use)
             require('gitsigns').setup()
         end
     }
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('mbbill/undotree')
-    use('tpope/vim-fugitive')
     use('folke/trouble.nvim')
+    use('jose-elias-alvarez/null-ls.nvim')
     use {
         'VonHeikemen/lsp-zero.nvim',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
     }
     use('simrat39/rust-tools.nvim')
     use {
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup()
-      end
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup()
+        end
     }
 end)
-
